84cf64dc40df1afebd534bd3ccea3e3598a40c9d
David Burrows <david@imergent.com>
2019-03-06 17:14:10 +0000

    Add basic branding
    Add some basic global styles using styled-components, get EE colour constants from website, add EE logo to header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
 import React, { Component } from "react";
+import { createGlobalStyle } from "styled-components";
+import "styled-components/macro";
+import { Normalize } from "styled-normalize";
+import { colors } from "./constants/colors";
+import { ReactComponent as Logo } from "./assets/ee-logo.svg";
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+  
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
+  html, body, #root {
+    height: 100%;
+  }
+
+  body {
+    box-sizing: border-box;
+    padding: 10px;
+    background-color: ${colors.eeLightGrey}
+  }
+`;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">App Header</header>
+        <Normalize />
+        <GlobalStyle />
+        <header
+          className="App-header"
+          css={`
+            background-color: ${colors.eeBlue};
+            padding: 3px 1px 1px 4px;
+          `}
+        >
+          <Logo
+            css={`
+              height: 100px;
+            `}
+          />
+        </header>
       </div>
     );
   }
---
+++ src/constants/colors.js
@@ -0,0 +1,6 @@
+export const colors = {
+  eeGrey: "#373d45",
+  eeBlue: "#1795d4",
+  eeYellow: "#fed800",
+  eeLightGrey: "#eee"
+};
